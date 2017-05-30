bb5d22eda74cefaf8b3ef1ba0ac31e2f15209d39
Clavos <clabaq@gmail.com>
2017-05-30 11:57:00 +0200

    commit

--- formulaire/views.py
+++ formulaire/views.py
@@ -1,3 +1,12 @@
+#-*- coding: utf-8 -*-
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    titrePrincipal = """<h1><b>Visites du GeePs</h1></b>"""
+    titreFormulaire = """<h2><b>Information visiteur :</b><h2>"""
+    texteFooter = """<h2><b>Protection des informations nominatives</b></h2>
+    					"""
+    text = titrePrincipal + titreFormulaire
+    return HttpResponse(text)
